48da3c306e7ae8563293b321e073ea2f79398d16
PkHabba <7434063w>
2021-07-23 03:46:18 +0300

    Таблица переписана под Angular Material(список задач, а не под шаблон который был) Изменено отображение, структура таблицы

--- src/app/views/task/task.component.ts
+++ src/app/views/task/task.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Task} from '../../model/Task';
 import {DataHandlerService} from '../../service/data-handler.service';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-task',
@@ -9,17 +10,46 @@ import {DataHandlerService} from '../../service/data-handler.service';
 })
 export class TaskComponent implements OnInit {
 
+  // Поля для таблицы (что отображают данные из задачи - должны совпадать с названиями переменных класса)
+
+  private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+  private dataSource: MatTableDataSource<Task>; // Источник данных для таблицы
+
   tasks: Task[];
 
   constructor(private dataHandler: DataHandlerService) {
   }
 
-  ngOnInit(): void {
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
     this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+// датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник
+    this.dataSource = new MatTableDataSource();
+
+    this.refreshTable();
   }
 
   // tslint:disable-next-line:typedef
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+// tslint:disable-next-line:typedef
+  private getPriopityColor(task: Task) {
+    if (task.completed) {
+      return '#F8F9FA';
+    }
+
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+  }
+
+  // Показывает задачи с применением всех текущих условий
+  // tslint:disable-next-line:typedef
+  private refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 }
